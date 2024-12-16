88a77351a1c8c0bfa3256de4d0c3b8e9f8556c04
Yim_Li <yan95501@outlook.com>
2024-12-16 08:53:57 +0800

    feat: 创建仓库

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+import Header from '@/components/Header.vue'
+</script>
+
+<template>
+  <Header></Header>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
